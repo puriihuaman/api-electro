5825ecbab31b9a0f24c6ae0214c69ad452500177
Pedro Purihuaman <pedropuriihuaman@gmail.com>
2024-12-20 00:08:02 -0500

    feat: Api electro

---
+++ src/main/java/purihuaman/ApiElectroApplication.java
@@ -0,0 +1,13 @@
+package purihuaman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiElectroApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ApiElectroApplication.class, args);
+  }
+
+}
---
+++ src/test/java/purihuaman/ApiElectroApplicationTests.java
@@ -0,0 +1,13 @@
+package purihuaman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiElectroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
